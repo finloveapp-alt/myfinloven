08efd9b52409cf38bc9229db9bf4e40fb2ccf213
joaofrancadantas <joaovytordantasdefranca@gmail.com>
2025-05-28 22:44:58 -0300

    Reduzido ainda mais o espaço abaixo do seletor de mês

--- app/(app)/receitas.tsx
+++ app/(app)/receitas.tsx
@@ -1544,7 +1544,7 @@ const createStyles = (theme: any) => StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 12,
-    paddingBottom: 8,
+    paddingBottom: 4,
   },
   monthArrow: {
     padding: 8,
